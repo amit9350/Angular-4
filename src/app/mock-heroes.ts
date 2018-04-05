f5a717a367dfa3585f4028c61fd9930c9c05e3b2
amit9350 <amitsonusingh66@gmail.com>
2018-04-05 15:58:09 +0530

    listing update

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,11 +1,11 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
+  { id: 11, name: 'Mr. Prach' },
+  { id: 12, name: 'Nanhki' },
+  { id: 13, name: 'Tarun' },
+  { id: 14, name: 'Akhand' },
+  { id: 15, name: 'Bhalal' },
   { id: 16, name: 'RubberMan' },
   { id: 17, name: 'Dynama' },
   { id: 18, name: 'Dr IQ' },
